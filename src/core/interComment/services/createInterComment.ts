ee3e8a2e20647b5d1e880095ce92ce6181eb44a5
QuesoCaliente <brianc.contacto@gmail.com>
2025-02-04 10:07:30 -0300

    fix createdAt

--- src/core/interComment/services/createInterComment.ts
+++ src/core/interComment/services/createInterComment.ts
@@ -5,12 +5,13 @@ import { InterComment, InterCommentType } from "../types/interface"
 export const createInterComment = async (request: FastifyRequest<{
     Body: InterCommentType
 }>, reply: FastifyReply) => {
-    const body = request.body
+    const body = request.body as InterCommentType
+    const {createdAt, ...rest} = body
     console.log(body)
     if (!body) return { message: "Missing data" }
     try {
         const interComment = await prisma.interComment.create({
-            data: body as InterCommentType,
+            data: rest as InterCommentType,
         })
         reply.status(201).send(interComment)
     } catch (error) {
